dd21bc7d1138567e9501149e420eb1afef623f00
Skanderchekir <skander.chekir@esprit.tn>
2024-11-13 23:39:31 -0100

    commit up

--- src/main/java/com/example/gestionstages/controllers/CandidatureRestAPI.java
+++ src/main/java/com/example/gestionstages/controllers/CandidatureRestAPI.java
@@ -3,13 +3,12 @@ package com.example.gestionstages.controllers;
 import com.example.gestionstages.entities.Stage;
 import com.example.gestionstages.services.IServiceStage;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+
+@RequestMapping("Stages")
 @RestController
 public class CandidatureRestAPI {
     private String title="hello, im the candidature MicroService";
