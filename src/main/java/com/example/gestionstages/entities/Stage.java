97ef5dd096e7c633d82874bf495c3363073b8eac
Skanderchekir <skander.chekir@esprit.tn>
2024-11-13 23:27:01 -0100

    commit changes

---
+++ src/main/java/com/example/gestionstages/entities/Stage.java
@@ -0,0 +1,20 @@
+package com.example.gestionstages.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.Date;
+@Entity
+public class Stage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String companyName;
+    private Date startDate;
+    private Date endDate;
+    private String subject;
+    private String level;
+}
