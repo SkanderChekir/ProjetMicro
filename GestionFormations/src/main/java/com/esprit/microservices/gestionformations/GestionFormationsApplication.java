99d28d9b7524c7ed5ba7e2425f8e180db5e04d79
Skanderchekir <skander.chekir@esprit.tn>
2024-11-13 23:50:51 -0100

    Commit last

--- GestionFormations/src/main/java/com/esprit/microservices/gestionformations/GestionFormationsApplication.java
+++ GestionFormations/src/main/java/com/esprit/microservices/gestionformations/GestionFormationsApplication.java
@@ -2,8 +2,10 @@ package com.esprit.microservices.gestionformations;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GestionFormationsApplication {
 
     public static void main(String[] args) {
